57fba2b530b89ef5a997f24e1e58e718d990b1c2
David Kalnischkies <david@kalnischkies.de>
2017-03-28 12:31:55 +0200

    do proper wrap dance in example mirco framework
    There are various situations (multiple host matches, back-buttons, …) in
    which a content script is executed multiple times on a page causing
    redeclaration errors if we aren't slightly more careful.

--- examples/FRAMEWORK/david.js
+++ examples/FRAMEWORK/david.js
@@ -1,41 +1,44 @@
-const $ = selector => document.querySelector(selector);
-const $$ = selector => document.querySelectorAll(selector);
-const forIt = (selector, callback) => {
-	const node = document.querySelector(selector);
-	if (node !== null)
-		callback(node);
-};
-const forEach = (selector, callback) => {
-	const array = typeof selector === 'string' ? document.querySelectorAll(selector) : selector;
-	for (let i = 0; i < array.length; ++i)
-		callback(array[i], i, array);
-};
-const addDetachListener = handler => {
-	browser.runtime.onMessage.addListener(l => {
-		if (l.cmd !== 'detach') return;
-		handler();
-	});
-};
-const addEventListener = (element, type, callback) => {
-	if (element !== null) {
-		addDetachListener(() => element.removeEventListener(type, callback));
-		element.addEventListener(type, callback);
-	}
-};
-const toggleStyle = (node, style, defvalue, flipvalue) => {
-	if(node !== null) {
-		const attrib = 'data-dotpagemod-togglestyle-' + style;
-		if (node.getAttribute(attrib) === null) {
-			addDetachListener(() => node.style[style] = node.getAttribute(attrib));
-			node.setAttribute(attrib, defvalue);
+(function(obj) {
+	"use strict";
+	obj.$ = selector => document.querySelector(selector);
+	obj.$$ = selector => document.querySelectorAll(selector);
+	obj.forIt = (selector, callback) => {
+		const node = document.querySelector(selector);
+		if (node !== null)
+			callback(node);
+	};
+	obj.forEach = (selector, callback) => {
+		const array = typeof selector === 'string' ? document.querySelectorAll(selector) : selector;
+		for (let i = 0; i < array.length; ++i)
+			callback(array[i], i, array);
+	};
+	obj.addDetachListener = handler => {
+		browser.runtime.onMessage.addListener(l => {
+			if (l.cmd !== 'detach') return;
+			handler();
+		});
+	};
+	obj.addEventListener = (element, type, callback) => {
+		if (element !== null) {
+			obj.addDetachListener(() => element.removeEventListener(type, callback));
+			element.addEventListener(type, callback);
 		}
-		node.style[style] = (window.getComputedStyle(node)[style] === defvalue) ? flipvalue : defvalue;
-	}
-};
-const runOnLoad = func => {
-	if (document.readyState === 'complete') {
-		func();
-	} else
-		addEventListener(document, 'readystatechange', e => { if (document.readyState === 'complete') func(e); });
-};
-addDetachListener(() => forEach('.dotpagemod-delete', itm => itm.parentNode.removeChild(itm)));
+	};
+	obj.toggleStyle = (node, style, defvalue, flipvalue) => {
+		if(node !== null) {
+			const attrib = 'data-dotpagemod-togglestyle-' + style;
+			if (node.getAttribute(attrib) === null) {
+				obj.addDetachListener(() => node.style[style] = node.getAttribute(attrib));
+				node.setAttribute(attrib, defvalue);
+			}
+			node.style[style] = (window.getComputedStyle(node)[style] === defvalue) ? flipvalue : defvalue;
+		}
+	};
+	obj.runOnLoad = func => {
+		if (document.readyState === 'complete') {
+			func();
+		} else
+			obj.addEventListener(document, 'readystatechange', e => { if (document.readyState === 'complete') func(e); });
+	};
+	obj.addDetachListener(() => forEach('.dotpagemod-delete', itm => itm.parentNode.removeChild(itm)));
+})(window);
