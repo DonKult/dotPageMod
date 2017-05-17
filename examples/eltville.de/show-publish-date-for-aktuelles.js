e8bc8a6c385dd25965433df7edb6cfb96ec1957c
David Kalnischkies <david@kalnischkies.de>
2017-05-17 15:54:08 +0200

    examples: eltville.de includes publish date by default now

--- examples/eltville.de/show-publish-date-for-aktuelles.js
+++
@@ -1,8 +0,0 @@
-forEach('div.inhalt > div.block > p.navigation', nav => {
-	if (nav.childNodes[1].nodeType === Node.COMMENT_NODE) {
-		const dt = document.createElement('time');
-		dt.innerText = nav.childNodes[1].textContent;
-		dt.classList.add('dotpagemod-delete');
-		nav.appendChild(dt);
-	}
-});
