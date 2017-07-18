74fa012c16f9699898d5826ccb7ba80a32b47b5e
David Kalnischkies <david@kalnischkies.de>
2017-07-18 10:00:06 +0200

    examples: zeit.de: show images without js and noscript
    They have <noscript> tags to show the images in case javascript is
    disabled, which pushes them way ahead of the crowd – but I don't have
    javascript disabled, so it doesn't work in my setup (not their fault!).
    As it is easy enough to add the image "by hand" I do that instead of
    inserting the whole <noscript> content as HTML into the page.

---
+++ examples/zeit.de/show-images.js
@@ -0,0 +1,11 @@
+forEach('figure', fig => {
+	if (fig.classList.contains('nextread-advertisement__media'))
+		return;
+	const url = fig.querySelector('meta[itemprop="url"]');
+	if (url === null)
+		return;
+	let img = document.createElement('img');
+	img.src = url.content;
+	img.classList.add('dotpagemod-delete');
+	fig.insertBefore(img, fig.firstChild);
+});
