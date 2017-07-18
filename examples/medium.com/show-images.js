f4cc6563ee21df6f8351a2f57ed0b0552eb526e9
David Kalnischkies <david@kalnischkies.de>
2017-07-18 09:56:19 +0200

    examples: medium.com: remove one level of img indirection

--- examples/medium.com/show-images.js
+++ examples/medium.com/show-images.js
@@ -3,7 +3,16 @@
 const MAXSIZE = 640;
 forEach('img.progressiveMedia-thumbnail', img => {
 	const id = img.parentNode.getAttribute('data-image-id');
-	if (typeof id !== "string")
-		return
-	img.src = 'https://cdn-images-1.medium.com/freeze/max/' + MAXSIZE + '/' + id;
+	if (typeof id === "string")
+		img.src = 'https://cdn-images-1.medium.com/freeze/max/' + MAXSIZE + '/' + id;
+	else if (img.src.startsWith('https://i.embed.ly/')) {
+		let start = img.src.indexOf('url=');
+		if (start === -1)
+			return;
+		start += 4;
+		let end = img.src.indexOf('&', start);
+		if (end === -1)
+			return;
+		img.src = decodeURIComponent(img.src.substr(start, end - start));
+	}
 });
