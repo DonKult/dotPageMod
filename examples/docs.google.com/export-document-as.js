cf7c86abe9b909f6af7413e5002c6bd018c03b3e
David Kalnischkies <david@kalnischkies.de>
2017-12-20 12:55:54 +0100

    examples: inline display of exported google docs presentations

--- examples/docs.google.com/export-document-as.js
+++ examples/docs.google.com/export-document-as.js
@@ -1,4 +1,4 @@
-const trigger = '/document/d/';
+let trigger = '/document/d/';
 if (window.location.pathname.startsWith(trigger)) {
 	const id = window.location.pathname.substr(trigger.length).split('/')[0];
 
@@ -16,11 +16,29 @@ if (window.location.pathname.startsWith(trigger)) {
 	body.insertBefore(p, body.firstChild);
 }
 
+trigger = '/presentation/d/';
+if (window.location.pathname.startsWith(trigger) && window.location.pathname.includes('/export/') === false) {
+	const id = window.location.pathname.substr(trigger.length).split('/')[0];
+
+	let p = document.createElement('p');
+	p.textContent = 'Export the presentation as';
+	p.classList.add('dotpagemod-delete');
+	[ 'pdf', 'pptx', 'odp', 'txt' ].forEach(format => {
+		let a = document.createElement('a');
+		a.href = 'https://docs.google.com/presentation/d/' + id + '/export/' + format + '?id=' + id;
+		a.textContent = format;
+		a.style['margin-left'] = '1em';
+		p.appendChild(a);
+	});
+	let body = $('body');
+	body.insertBefore(p, body.firstChild);
+}
 /* The exports are fashioned as downloads with the usual save dialog by firefox,
    but with the following code in a background script (*not* in a content script
    like this one) of an extension you can cause the default handler to step in.
    In the case of PDF you will e.g. browse the file in PDF.js instead of saving it
 
+(function() {
 function dispositionInlineForGoogleDocuments(e) {
 	e.responseHeaders.forEach(o => {
 		if (o.name.toLowerCase() !== 'Content-Disposition'.toLowerCase())
@@ -33,7 +51,11 @@ function dispositionInlineForGoogleDocuments(e) {
 }
 browser.webRequest.onHeadersReceived.addListener(
 	dispositionInlineForGoogleDocuments,
-	{urls: ['https://docs.google.com/document/export?id=*'] },
+	{urls: [
+		'https://docs.google.com/document/export?id=*',
+		'https://docs.google.com/presentation/d/*' + '/export/*?id=*'
+	], types: ['main_frame'] },
 	["blocking", "responseHeaders"]
 );
+})();
 */
